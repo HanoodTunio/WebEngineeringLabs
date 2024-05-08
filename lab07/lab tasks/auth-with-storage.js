6e58dc8aadafc1766b477f685a79717fe7b5fd54
HanoodTunio <hanood.tunio@gmail.com>
2024-05-08 13:21:52 +0500

    Implemented Sign-out Functionality

--- lab07/lab tasks/auth-with-storage.js
+++ lab07/lab tasks/auth-with-storage.js
@@ -1,6 +1,7 @@
 const username = document.getElementById('username');
 const password = document.getElementById('password');
 const loginButton = document.getElementById('login');
+const signoutButton = document.getElementById('signout');
 
 // Retrieve the stored user object from local storage
 const storedJSON = localStorage.getItem('user');   
@@ -16,9 +17,16 @@ loginButton.addEventListener('click', () => {
         // Check if the entered username and password match the stored user
     if (enteredUsername === storedUser.username && enteredPassword === storedUser.password) {
         alert("Successfully logged in");
+        window.location.href = 'home.html';
     } 
    else {
         alert("Incorrect username or password");
     }
-    
+
 });
+
+
+signoutButton.addEventListener('click', () => {
+    localStorage.clear();
+    window.location.href = "/";
+})
