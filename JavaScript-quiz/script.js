7b5b19c2a7220e9e17726ea5921f5bfedf7a3d17
HanoodTunio <hanood.tunio@gmail.com>
2024-05-11 03:23:16 +0500

    Completed HTML and CSS for quiz section

---
+++ JavaScript-quiz/script.js
@@ -0,0 +1,20 @@
+const startBtn = document.querySelector('.start-btn');
+const popupInfo = document.querySelector('.popup-info');
+const exitBtn = document.querySelector('.exit-btn');
+const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizSection = document.querySelector('.quiz-section');
+
+startBtn.onclick = () =>{
+    popupInfo.classList.add('active');
+    main.classList.add('active');
+}
+
+exitBtn.onclick = () =>{
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+}
+
+continueBtn.onclick = () => {
+    window.location.href = "quiz-section.html";
+}
